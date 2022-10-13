e81e3be3e0bef117e3daa3c63424bcc32f415a5b
Татьяна Языкова <tatiana-iazykova@fox-macbook-pro.local>
2022-10-13 22:46:47 +0300

    update package version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="bert-for-sequence-classification",
-    version='0.0.3',
+    version='0.0.4',
     author="Tatiana Iazykova",
     author_email="tania_yazykova@bk.ru",
     description='Easy fine-tuning for BERT models',
